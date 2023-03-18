a6a70bc0405c5832d1bf0eda264694ab597ef4d8
axelcc123 <125621372+axelcc123@users.noreply.github.com>
2023-03-18 12:47:36 -0600

    index.html

---
+++ sketch.js
@@ -0,0 +1,28 @@
+
+
+
+function preload(){
+
+  seaImg = loadImage("sea.png");
+shipImg1 = loadAnimation //enlista los nombres de los png que desees);
+spriteName.addImage(seaImg);
+//codigo para reiniciar el fondo
+if (sea.x <0){
+  sea.x=sea.widt/2
+}
+  shipImg1 = loadAnimation("ship-1.png")
+  shipImg2 = loadAnimation("ship-2.png")
+  shipImg3 = loadAnimation("ship-3.png")
+  shipImg4 = loadAnimation("ship-4.png")
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+ 
+}
